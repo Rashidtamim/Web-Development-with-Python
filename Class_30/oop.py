d0d9b068676f7eee2f842607729d0054f201529c
Rashid Tamim <rashidtamim999@gmail.com>
2025-11-26 13:24:38 +0600

    Class_30

--- Class_30/oop.py
+++ Class_30/oop.py
@@ -1,25 +1,3 @@
-class Car:
-    color = "black"#class attribute
-    brand = "Tesla"#class attribute  #whatever we write inside the car this all will be object and this is called as a blueprint
-    model = "premio"#class attribute
-    manufactured = 2016
-    def __init__(self, c="Yellow",b ="Audi",m = "Q7"):
-        self.color=c  #instance attribute
-        self.brand=b  #instance attribute
-        self.model=m   #instance attribute
-    def __str__(self):
-        return f"{self.color} {self.brand} {self.model}"
-
-
-car1 = Car()
-car2 = Car()
-# car1.color = "White"
-# car1.brand = "Axio"
-# car1.model = "Rickshay Tesla"
-print(car1)
-# print(car2)
-
-
 class Animal:
     Cat = "SweetCat"
     Dog = "Kutta"
