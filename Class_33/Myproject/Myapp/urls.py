f033a6d3403ebad93b3599404ffae8effefe3e02
Rashid Tamim <rashidtamim999@gmail.com>
2025-11-30 13:33:24 +0600

    Class_33_Django_first_page

--- Class_33/Myproject/Myapp/urls.py
+++ Class_33/Myproject/Myapp/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('home/', home)
+    path('', home, name='home'),
+    path('about/', about, name="about"),
+    path('news/', news, name="news"),
 ]
--- Class_33/Myproject/Myapp/views.py
+++ Class_33/Myproject/Myapp/views.py
@@ -6,3 +6,9 @@ from django.http import HttpResponse
 
 def home(request):
    return render(request, "index.html")
+
+def about(request):
+   return render(request, "about.html")
+
+def news(request):
+   return render(request, "news.html")
