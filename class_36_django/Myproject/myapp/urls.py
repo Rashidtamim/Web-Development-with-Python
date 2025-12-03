4356283c9292729e233cda9ce2a92cfe8c7b3899
Rashid Tamim <rashidtamim999@gmail.com>
2025-12-03 23:47:26 +0600

    loginsignup_django

--- class_36_django/Myproject/myapp/urls.py
+++ class_36_django/Myproject/myapp/urls.py
@@ -3,7 +3,8 @@ from .import views
 from .views import *
 
 urlpatterns = [
-    path('',views.index,name='home' ),
+    path('',views.index,name='index' ),
+    path('home/',views.home,name='home' ),
     path('news/',views.news,name='news1' ),
     path('about/',views.about,name='about1' ),
     path('contact/',views.contact,name='contact1' ),
--- class_36_django/Myproject/myapp/views.py
+++ class_36_django/Myproject/myapp/views.py
@@ -11,6 +11,8 @@ def home(request):
 from django.shortcuts import render
 
 def index(request):
+    return render(request, 'index.html')
+def home(request):
     return render(request, 'home.html')
 
 def news(request):
