2492ad1d2f745a60ed7efe1c69ba1fdf9d3e64b3
Rashid Tamim <rashidtamim999@gmail.com>
2025-12-04 13:12:54 +0600

    Class_37_home_contact_login_register

---
+++ Class_37_login_page/Myproject/myapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .import views 
+from .views import *
+
+urlpatterns = [
+    path('',views.index,name='index' ),
+    path('home/',views.home,name='home' ),
+    path('news/',views.news,name='news1' ),
+    path('about/',views.about,name='about1' ),
+    path('contact/',views.contact,name='contact1' ),
+    path('login/',views.login,name='login1' ),
+]
