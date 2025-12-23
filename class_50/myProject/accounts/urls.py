38930d1b1c57e6ff8ced8bd2d250b43357e79856
tamim <rrtamim@gmail.com>
2025-12-23 13:10:12 +0600

    class_51_login

--- class_50/myProject/accounts/urls.py
+++ class_50/myProject/accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .import views
+
+from . import views
 
 urlpatterns = [
-    
-    path('',views.homepage,),
-    path('register/',views.register, name='register'),
-    path('login/',views.user_login,name='user_login'),
-
+    path('', views.home,),
+    path('register/', views.register, name='register'),
+    path('user_login/', views.user_login, name='user_login'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('user_logout/', views.user_logout, name='user_logout'),
 ]
--- class_50/myProject/accounts/views.py
+++ class_50/myProject/accounts/views.py
@@ -1,30 +1,59 @@
-from django.shortcuts import render,redirect
-from django.contrib.auth import authenticate,login,logout
+from django.shortcuts import redirect, render
+
+from django.http import HttpResponse
+
+from django.contrib.auth import authenticate, login, logout
 
 from django.contrib.auth.models import User
 
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
-def homepage(request):
-    return render(request,'index.html')
+def home(request):
+    return render(request, 'index.html')
 
 
 def register(request):
-
+    
     if request.method == "POST":
-        User.object.create_user(
-            username = request.POST.get("username"),
-            password = request.POST.get("password"),
+        User.objects.create_user(
+            username = request.POST.get('username'),
+            password = request.POST.get('password')
         )
+        
         return redirect('user_login')
-    return render(request,'auth/register.html')
+        
+    
+    return render(request, 'auth/registration.html')
 
 
 def user_login(request):
-    return render(request,'auth/login.html')
+    
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username = username, password = password)
+        
+        if user:
+            login(request, user)
+            
+            if user.is_superuser:
+                return redirect('/admin/')
+            else:
+                return redirect('dashboard')
+        else:
+            return HttpResponse("Invalid Username or Password!!!")
+    
+    return render(request, 'auth/login.html')
 
 
-
-
-
-
+def user_logout(request):
+    logout(request)
+    return redirect('user_login')
+    
+@login_required
+def dashboard(request):
+    
+    return render(request, 'dashboard.html')
