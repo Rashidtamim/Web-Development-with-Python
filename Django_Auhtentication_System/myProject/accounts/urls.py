469ffe41f4f9044521e3b8678be351921f333b75
rrtamim <rashidtamim999@gmail.com>
2025-12-29 13:17:24 +0600

    login_register

--- Django_Auhtentication_System/myProject/accounts/urls.py
+++ Django_Auhtentication_System/myProject/accounts/urls.py
@@ -3,5 +3,7 @@ from .import views
 
 urlpatterns = [
     path('',views.home, name='home'),
-    path('login/',views.user_login,name='log')
+    path('login/',views.user_login,name='log'),
+    path('register/',views.user_register,name='reg'),
+    path('dashboard/',views.dashboard,name='dash'),
 ]
--- Django_Auhtentication_System/myProject/accounts/views.py
+++ Django_Auhtentication_System/myProject/accounts/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout,update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -8,3 +15,23 @@ def home(request):
 
 def user_login(request):
     return render(request,'auth/login.html')
+
+def user_register(request):
+
+    if request.method == 'POST':
+        user = User.objects.create_user(
+            username = request.POST.get('username'),
+            email = request.POST.get('email'),
+            password = request.POST.get('password'),
+
+        )
+        user.first_name = request.POST.get('Firstname')
+        user.last_name = request.POST.get('Lastname')
+
+        user.save()
+        return redirect('log')
+
+    return render(request,'auth/register.html')
+
+def dashboard(request):
+    return render(request,'protected/dashboard.html')
